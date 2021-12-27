12c92ce0afbf1f976afb831e374024a7fb22aac3
MD Fozle Rabbi Piyash <54602251+MDFOZLERABBIPIYASH@users.noreply.github.com>
2021-12-27 22:41:56 +0600

    update

--- update.php
+++ update.php
@@ -3,13 +3,13 @@
 
     if(isset($_POST['update'])){
         $firstname = $_POST['firstname'];
-        $user_id = $_POST['id'];
+        $user_id = $_POST['user_id'];
         $lastname = $_POST['lastname'];
         $email = $_POST['email'];
         $password = $_POST['password'];
         $gender = $_POST['gender'];
 
-        $sql = "UPDATE users SET 'firstname'= '$firstname', 'lastname' = '$lastname', 'email'='$email', 'password'='$password', 'gender'= 'gender' WHERE 'id'='$user_id' ";
+        $sql = "UPDATE users SET firstname=$firstname, lastname=$lastname, email=$email, password=$password, gender=$gender WHERE id=$user_id";
         $result = $conn->query($sql);
         if($result == true){
             echo "Record Updated Succesfull";
@@ -20,7 +20,7 @@
     }
     if(isset($_GET['id'])){
         $user_id = $_GET['id'];
-        $sql = "SELECT *FROM 'uesrs' WHERE 'id'='$user_id' ";
+        $sql = "SELECT *FROM users WHERE id=$user_id";
         $result = $conn->query($sql);
 
         if($result->num_rows>0){
@@ -46,7 +46,7 @@
             <legend>Personal Information</legend>
             First Name:<br>
             <input type="text" name="firstname" value="<?php echo $firstname; ?>"><br>
-            <input type="hidden" name="user_id" value="<?php echo $id; ?>"><br>
+            <input type="hidden" name="user_id" value="<?php echo $user_id; ?>"><br>
             last Name:<br>
             <input type="text" name="lastname" value="<?php echo $lastname; ?>"><br>
             E-mail:<br>
@@ -56,8 +56,8 @@
             <input type="password" name="password" value="<?php echo $password; ?>"><br>
             
             Gender:<br>
-            <input type="radio" name="gender" value="Male" <?php if($gender == 'Male'){echo "checked";} ?> >Male
-            <input type="radio" name="gender" value="Female" <?php if($gender == 'Female'){echo "checked";} ?>>Female<br><br>
+            <input type="radio" name="gender" value="Male" <?php if($gender == 'Male'){echo "checked";} ?> > Male
+            <input type="radio" name="gender" value="Female" <?php if($gender == 'Female'){echo "checked";} ?>> Female<br><br>
             <input type="submit" name="update" value="update">
         </fieldset>
         </form>
@@ -65,10 +65,9 @@
         </html>
         <?php
         }else{
-        //if  the value is not valid, it will redirect to "view.php" page
-        header('Location: viewpage.php');
+            //if  the value is not valid, it will redirect to "view.php" page
+            header('Location: viewpage.php');
         }
     }
 ?>
-
-
+        
